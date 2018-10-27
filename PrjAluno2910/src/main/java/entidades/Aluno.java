2a636005589aa2be0911d9456a1befeec8985868
Gabrieladmo <gabrieladmo@gmail.com>
2018-10-27 17:39:37 -0300

    Prj Aluno - Banco de dados - 29/10/2018

---
+++ PrjAluno2910/src/main/java/entidades/Aluno.java
@@ -0,0 +1,40 @@
+package entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno {
+	@Id
+	private int RA;
+	private String nome;
+	private String dtNascimento;
+	private String nomeMae;
+	
+	public int getRA() {
+		return RA;
+	}
+	public void setRA(int RA) {
+		this.RA = RA;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(String dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
+
+}
